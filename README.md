487e14292efdb60b24a718c701ec5a33df741b0f
Nisha Singhal <nisha@Nishas-MacBook-Air.local>
2025-05-05 04:27:05 +0530

    First upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# README
+This website is built using this [minimally changed fork of minimal mistakes](https://github.com/Arpit-Babbar/minimal-mistakes)
---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,37 @@
+var store = [{
+        "title": "Presentation at the Numerical Engine Room",
+        "excerpt":"I am about to give a talk in the Numerical Engine Room Talks (numerical-engine-room-talks.github.io)  series which is organized by the wonderful developers of Trixi.jl.  ","categories": [],
+        "tags": [],
+        "url": "/2023/10/26/talk/",
+        "teaser": null
+      },{
+        "title": "Talk at the Mathematics of Weather conference",
+        "excerpt":"I will be presenting some of my PhD work at the Mathematics of Weather (MOW) 2024.  ","categories": ["blog"],
+        "tags": ["Jekyll","update"],
+        "url": "/blog/2024/10/04/mow/",
+        "teaser": null
+      },{
+        "title": "Talk at Julia user group Mainz",
+        "excerpt":"I will be presenting my workflow using julia over SSH using VS code and Pluto in the Julia user group Mainz. The result of this workflow is an interactive environment where I can run my simulations and visualize them without having to leave VS Code.  ","categories": ["blog"],
+        "tags": ["Jekyll","update"],
+        "url": "/blog/2024/10/24/julia/",
+        "teaser": null
+      },{
+        "title": "Talk at the workshop on Hardware-aware Scientific Computing",
+        "excerpt":"I will be presenting my PhD work on admissibility preserving subcell limiters for LWFR schemes in IGHASC: Indo-german Workshop on Hardware-aware Scientific Computing. The paper I will be presenting is Admissibility Preserving Subcell Limiter for Lax–Wendroff Flux Reconstruction.  ","categories": ["blog"],
+        "tags": ["Jekyll","update"],
+        "url": "/blog/2024/10/25/iwr/",
+        "teaser": null
+      },{
+        "title": "Talk on Introduction to Trixi.jl",
+        "excerpt":"Marco Artiano and I gave an introduction to Trixi.jl in the Julia user group Mainz. The (reproducible) material we presented is available at github.com/ranocha/Julia_User_Group_Mainz/tree/main/2025-01-23__Introduction_To_Trixi. You can also get a preview on the browser     https://ranocha.de/Julia_User_Group_Mainz/2025_01_23_Introduction_To_Trixi_Part1.html   https://ranocha.de/Julia_User_Group_Mainz/2025_01_23_trixi_applications.html  ","categories": ["blog"],
+        "tags": ["Jekyll","update"],
+        "url": "/blog/2025/01/24/trixi/",
+        "teaser": null
+      },{
+        "title": "GPU codes using KernelAbstractions.jl",
+        "excerpt":"Marco Artiano and I wrote some GPU codes using KernelAbstractions.jl with the help of Valentin Churavy. This is our first time using KernelAbstractions.jl, and we will appreciate any feedback and comments!     https://github.com/Arpit-Babbar/KernelAbstractionsFV.jl   https://github.com/MarcoArtiano/IncompressibleNavierStokes.jl  ","categories": ["blog"],
+        "tags": ["Jekyll","update"],
+        "url": "/blog/2025/03/08/kernel/",
+        "teaser": null
+      }]
